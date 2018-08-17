3f7cace0f6162d14990de768810d3d08f8dde63e
kujan <fredrik.k91@gmail.com>
2018-08-17 23:58:12 +0200

    added requirements.txt

---
+++ ngucon.py
@@ -0,0 +1,138 @@
+NguResolutionX = 0
+NguResolutionY = 0
+EnergyAssigned = False
+MagicAssigned = False
+
+#menu offsets
+MenuOffsetX = 230
+FightBossMenuOffsetY = 75
+PitMenuOffsetY = 105
+AdventureMenuOffsetY = 135
+InventoryMenuOffsetY = 165
+AugmentationMenuOffsetY = 195
+AdvTrainingMenuOffsetY = 225
+TimeMachineMenuOffsetY = 255
+BloodMagicMenuOffsetY = 285
+WanadoosMenuOffsetY = 315
+NguMenuOffsetY = 345
+YggdrasilMenuOffsetY = 375
+BeardMenuOffsetY = 405
+NumberInputBoxX = 345
+NumberInputBoxY = 50
+
+#fight boss offsets
+
+NukeX = 620
+NukeY = 110
+FightX = 620
+FightY = 220
+
+#inventory offsets
+Accessory1OffsetX = 480
+Accessory1OffsetY = 65
+Accessory2OffsetX = 480
+Accessory2OffsetY = 112
+Accessory3OffsetX = 0
+Accessory4OffsetX = 0
+HeadOffsetX = 525
+HeadOffsetY = 65
+ChestOffsetX = 527
+ChestOffsetY = 114
+LegsOffsetX = 527
+LegsOffsetY = 163
+BootsOffsetX = 527
+BootsOffsetY = 212
+WeaponOffsetX = 575
+WeaponOffsety = 115
+CubeOffsetX = 627
+CubeOffsetY = 115
+
+#time machine offsets
+TmSpeedX = 532
+TmSpeedY = 233
+TmMultX = 532
+TmMultY = 330
+
+#blood magic offsets
+
+BmX = 495
+Bm1 = 228
+Bm2 = 263
+Bm3 = 298
+Bm4 = 333
+Bm5 = 369
+Bm6 = 403
+Bm7 = 438
+Bm8 = 473
+
+BmSpellsX = 395
+BmSpellsY = 115
+
+#Augmentation offsets
+AugmentX = 535
+AugmentScissorsY = 265
+AugmentDScissorsY = 292
+
+#Times = { Augmentations: 45, TimeMachine: 0, BloodMagic: 15, NGU: 40, AdvTraining: 40}
+#Ratios = { Augmentations: 1, TimeMachine: 1, BloodMagic: 1, NGU: 0, AdvTraining: 0.1}
+
+eCap = 35000000
+mCap = 1000000
+eSpeed = 13750
+mSpeed = 2500
+
+#NGU offsets
+
+NguX = 530
+Ngu1Y = 243
+Ngu2Y = 278
+Ngu3Y = 313
+Ngu4Y = 349
+Ngu5Y = 384
+Ngu6Y = 453
+
+#AdvTraining offsets
+
+AdvTrainingX = 890
+AdvTraining1Y = 230
+AdvTraining2Y = 270
+AdvTraining3Y = 310
+AdvTraining4Y = 350
+AdvTraining5Y = 390
+
+#yggdrasil offsets
+
+HarvestX = 814
+HarvestY = 450
+Fruit1X = 350
+Fruit1Y = 180
+Fruit2X = 560
+Fruit2Y = 180
+Fruit3X = 775
+Fruit3Y = 180
+Fruit4X = 350
+Fruit4Y = 270
+Fruit5X = 560
+Fruit5Y = 270
+Fruit6X = 775
+Fruit6Y = 270
+Fruit7X = 350
+Fruit7Y = 370
+Fruit8X = 560
+Fruit8Y = 370
+Fruit9X = 775
+Fruit9Y = 370
+
+#rebirth offsets
+RebirthX = 90
+RebirthY = 420
+RebirthButtonX = 545
+RebirthButtonY = 520
+RebirthConfirmX = 425 
+RebirthConfirmY = 320
+AdventureZone = 6
+
+#pit offsets
+
+PitConfirmX = 437
+PitConfirmY = 317
