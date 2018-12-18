f26dc6ed02590b6dd079f61196d71a4824084e7a
Jathan Haggard <jathan.haggard@genesys.com>
2018-12-18 13:57:12 -0500

    running file changes

--- Python/Scripts/5min.py
+++ Python/Scripts/5min.py
@@ -1,4 +1,4 @@
-"""12-minute rebirth script"""
+"""5-minute rebirth script"""
 
 # Challenges
 from challenges.basic import Basic
@@ -9,12 +9,15 @@ from classes.challenge import Challenge
 from classes.features import Features
 from classes.inputs import Inputs
 from classes.navigation import Navigation
-from classes.stats import Stats
+from classes.stats import Stats, EstimateRate, Tracker
 from classes.upgrade import Upgrade
 from classes.window import Window
 
+import ngucon as ncon
+import time
+
+
 def speedrun(duration, f):
-    #TODO: Add in debug statements 
     """Start a speedrun.
     Keyword arguments
     duration -- duration in minutes to run
@@ -23,57 +26,51 @@ def speedrun(duration, f):
     f.do_rebirth()
     start = time.time()
     end = time.time() + (duration * 60)
-    magic_assigned = False
-    augments_assigned = False
     blood_digger_active = False
-    f.fight()
-    #TODO: What is the point of this?
-    # current_boss = int(feature.get_current_boss())
-    # while current_boss < 60:
-    #     print(f"at boss {current_boss}, fighting {60-current_boss + 1} more times")
-    #     feature.fight(60-current_boss + 1)
-    #     current_boss = int(feature.get_current_boss())
-
+    itopod_advance = False
+    f.nuke()
     f.loadout(2)  # Gold drop equipment
     f.adventure(highest=True)
-    time.sleep(7)
+    time.sleep(5)
     f.loadout(1)  # Bar/power equimpent
     f.adventure(itopod=True, itopodauto=True)
     #Sleep so can get enough E/M for augments
-    time.sleep(45)
-    f.augments({"SS": 0.3, "DS": 0.7}, 5.5e6) #5.5m
+    time.sleep(5)
+    f.augments({"MI": 0.3, "DTMT": 0.7}, 2e7) #20m
     #Sleep so time machine can get some E/M
     #TODO: Optimize
-    time.sleep(45)
+    time.sleep(15)
+    f.blood_magic(4)
     f.time_machine(True)
-    
-    while time.time() < end - 15:
-        if time.time() > end - (duration * 0.85 * 60):
-            if not augments_assigned:
-                # f.send_string("r")
+    f.boost_equipment()
+    f.boost_cube()
+    f.gold_diggers([2, 3, 5, 6], True)
+    f.wandoos(True)
+    #Add turning on diggers here
 
-                augments_assigned = True
-                f.boost_equipment()
-        # Reassign magic from TM into BM after half the duration
-        if not magic_assigned and time.time() > end - (duration * 0.75 * 60):
-                f.menu("bloodmagic")
-                time.sleep(0.2)
-                f.blood_magic(2)
-                magic_assigned = True
-                f.wandoos(True)
-                # f.gold_diggers([2], True)
+    while time.time() < end - 20:
+        f.wandoos(True)
+        f.gold_diggers([2, 3, 5, 6])
+        # Don't have blood digger
+        # if time.time() > start + 40 and not blood_digger_active:
+        #     blood_digger_active = True
+        #     f.gold_diggers([11], True)
 
+        #Don't have enough to mess with NGU in short runs yet
+        if time.time () > start + 120:
+            try:
+                NGU_energy = int(f.remove_letters(f.ocr(ncon.OCR_ENERGY_X1, ncon.OCR_ENERGY_Y1, ncon.OCR_ENERGY_X2, ncon.OCR_ENERGY_Y2)))
+                feature.assign_ngu(NGU_energy, [1, 2, 5, 6])
+                NGU_magic = int(f.remove_letters(f.ocr(ncon.OCR_MAGIC_X1, ncon.OCR_MAGIC_Y1, ncon.OCR_MAGIC_X2, ncon.OCR_MAGIC_Y2)))
+                feature.assign_ngu(NGU_magic, [2, 3], magic=True)
+            except ValueError:
+                print("couldn't assign e/m to NGUs")
+            time.sleep(0.5)
 
-        """
-       if time.time() > end - (duration * 0.74 * 60):
-                                 try:
-                                     NGU_energy = int(feature.remove_letters(feature.ocr(ncon.OCR_ENERGY_X1,ncon.OCR_ENERGY_Y1,ncon.OCR_ENERGY_X2,ncon.OCR_ENERGY_Y2)))
-                                     feature.assign_ngu(NGU_energy, [1, 2, 4, 5, 6]) 
-                     
-                                     NGU_magic = int(feature.remove_letters(feature.ocr(ncon.OCR_MAGIC_X1, ncon.OCR_MAGIC_Y1, ncon.OCR_MAGIC_X2, ncon.OCR_MAGIC_Y2)))
-                                     feature.assign_ngu(NGU_magic, [1, 2], magic=True)
-                                 except ValueError:
-                                     print("couldn't assign e/m to NGUs")"""
+        if time.time() > start + 90 and not itopod_advance:
+            f.adventure(itopod=True, itopodauto=True)
+            itopod_advance = True
+
         # Do some early fights to open up furthest area for gold
         if time.time() < end - (duration * 0.75 * 60):
             #Make sure we're making maximum gold as the run progresses
@@ -81,63 +78,46 @@ def speedrun(duration, f):
             time.sleep(7)
             f.adventure(itopod=True, itopodauto=True)
 
-        # Assign leftovers into wandoos, turn on digger, fight for boss
-        if augments_assigned:
-            f.wandoos(True)
-            # f.gold_diggers([2], True)
-            f.fight()
-
         # if time.time() > start + 120 and not blood_digger_active:
         #     blood_digger_active = True
             # f.gold_diggers([11], True)
 
         time.sleep(0.5)
 
-    f.gold_diggers([3], True)
+    time.sleep(2)
+    f.nuke()
+    time.sleep(2)
     f.fight()
+    time.sleep(7)
     f.pit()
     f.spin()
-    time.sleep(7)
+    f.save_check()
+    tracker.progress()
+    u.em()
+    tracker.adjustxp()
     f.speedrun_bloodpill()
+    while time.time() < end:
+        time.sleep(0.1)
 
-import time
+    return
 
 w = Window()
 i = Inputs()
 nav = Navigation()
 feature = Features()
-c = Challenge()
 
-Window.x, Window.y = i.pixel_search("212429", 0, 0, 400, 600)
+Window.x, Window.y = i.pixel_search(ncon.TOP_LEFT_COLOR, 0, 0, 400, 600)
 nav.menu("inventory")
-s = Stats()
-u = Upgrade(37500, 37500, 1, 1, 3)
-# How much XP is this?
+
+u = Upgrade(37500, 37500, 2, 2, 3)
 
 print(w.x, w.y)
-
+tracker = Tracker(5)
 count = 0
 
-while count <= 11: # Adjust # for how many runs you want to do during testing minus 1
-    count += 1
+# while count <= 11: # Adjust # for how many runs you want to do during testing minus 1
+#     count += 1
+#     speedrun(5, feature)
+
+while True:  # main loop to go foreverrrrr
     speedrun(5, feature)
-    s.print_exp()
-    u.em()
-
-
-# while True:  # main loop to go foreverrrrr
-    # count += 1
-#     feature.merge_equipment()
-    # feature.boost_equipment()
-#     feature.merge_inventory()
-#     feature.boost_inventory()
-#     feature.adventure(zone=13, highest=False, itopod=False, itopodauto=False)
-#     feature.ygg()
-#     time.sleep(120)
-    # speedrun(5, feature)
-    # s.print_exp()
-    # u.em()
-
-
-
-s.print_run_done(count)
--- Python/Scripts/merge_and_boost.py
+++ Python/Scripts/merge_and_boost.py
@@ -3,12 +3,20 @@ Merge and Boost script
 No Transformation at this time.
 """
 
+# Challenges
+from challenges.basic import Basic
+from challenges.level import Level
+
 # Helper classes
+from classes.challenge import Challenge
 from classes.features import Features
 from classes.inputs import Inputs
 from classes.navigation import Navigation
+from classes.stats import Stats, EstimateRate, Tracker
+from classes.upgrade import Upgrade
 from classes.window import Window
 
+import ngucon as ncon
 import time
 
 w = Window()
@@ -16,17 +24,32 @@ i = Inputs()
 nav = Navigation()
 feature = Features()
 
-Window.x, Window.y = i.pixel_search("212429", 0, 0, 400, 600)
+Window.x, Window.y = i.pixel_search(ncon.TOP_LEFT_COLOR, 0, 0, 400, 600)
 nav.menu("inventory")
+
+u = Upgrade(37500, 37500, 2, 2, 3)
 
 print(w.x, w.y)
 
 while True:  # main loop to go foreverrrrr
     feature.merge_equipment()
     feature.boost_equipment()
-    feature.merge_inventory()
-    feature.boost_inventory()
+    feature.merge_inventory(12)
+    feature.boost_inventory(12)
+    feature.boost_cube()
     #Sets you to your designed zone in case you get bumped out somehow
-    feature.adventure(zone=13, highest=False, itopod=False, itopodauto=False)
+    # feature.adventure(zone=13, highest=False, itopod=False, itopodauto=False)
+
+    #Add Money Pit Check
+    feature.pit()
+    #Add Save Check
+    feature.save_check()
+    #Add Spin Check
+    feature.spin()
+    #Add Blood Magic Check
+    feature.speedrun_bloodpill()
+    #Ygg
     feature.ygg()
+    #Sleep before trying again
     time.sleep(120)
+    # feature.snipe(18, 300, once=False, highest=False, bosses=True)
