bf2815652b67cfc2ce4ac233cdddc5f88728c4f6
Jathan Haggard <jathan.haggard@genesys.com>
2018-12-18 14:36:48 -0500

    Adding in alternate feature fixes for Windows Server

---
+++ Python/Scripts/classes/alt_features.py
@@ -0,0 +1,64 @@
+"""Feature class handles the alternative features in the game for Windows Server."""
+from classes.inputs import Inputs
+from classes.navigation import Navigation
+from classes.window import Window
+from collections import deque, namedtuple
+from decimal import Decimal
+import math
+import ngucon as ncon
+import re
+import time
+import win32con as wcon
+import win32gui
+import usersettings as userset
+
+class AltFeatures(Navigation, Inputs):
+    """Handles the Windows 2018 different features in the game."""
+
+     def alt_merge_equipment(self):
+        """Navigate to inventory and merge equipment."""
+        self.menu("inventory")
+        time.sleep(0.5)
+        for slot in self.equipment:
+            if (slot == "cube"):
+                return
+            self.d_click(self.equipment[slot]["x"], self.equipment[slot]["y"])
+
+    def alt_boost_equipment(self):
+        """Boost all equipment."""
+        self.menu("inventory")
+        time.sleep(0.5)
+        for slot in self.equipment:
+            if (slot == "cube"):
+                return
+            self.a_click(self.equipment[slot]["x"], self.equipment[slot]["y"])
+    def alt_merge_inventory(self, slots):
+        """Merge all inventory slots starting from 1 to slots.
+
+        Keyword arguments:
+        slots -- The amount of slots you wish to merge
+        """
+        self.menu("inventory")
+        coords = self.get_inventory_slots(slots)
+        for slot in coords:
+            self.d_click(slot.x, slot.y)
+
+    def alt_boost_inventory(self, slots):
+        """Merge all inventory slots starting from 1 to slots.
+
+        Keyword arguments:
+        slots -- The amount of slots you wish to merge
+        """
+        self.menu("inventory")
+        coords = self.get_inventory_slots(slots)
+        for slot in coords:
+            self.a_click(slot.x, slot.y)
+            
+    def boost_cube(self):
+        """Boost cube."""
+        self.menu("inventory")
+        for slot in self.equipment:
+            if (slot == "cube"):
+                self.click(self.equipment[slot]["x"],
+                            self.equipment[slot]["y"], "right")
+                return
---
+++ Python/Scripts/classes/alt_inputs.py
@@ -0,0 +1,53 @@
+"""Input class contains alternative functions for mouse and keyboard input on Windows Server."""
+from classes.window import Window as window
+from ctypes import windll
+from PIL import Image as image
+from PIL import ImageFilter
+import cv2
+import ngucon as ncon
+import usersettings as userset
+import numpy
+import pytesseract
+import re
+import time
+import os
+import sys
+import win32api
+import win32con as wcon
+import win32gui
+import win32ui
+
+class AltInputs():
+    def a_click(self, x, y):
+        x += window.x
+        y += window.y
+        lParam = win32api.MAKELONG(x, y)
+        # MOUSEMOVE event is required for game to register clicks correctly
+        win32gui.PostMessage(window.id, wcon.WM_MOUSEMOVE, 0, lParam)
+        #Left click then A
+        win32gui.PostMessage(window.id, wcon.WM_LBUTTONDOWN,
+                                wcon.MK_LBUTTON, lParam)
+        # win32api.keybd_event(0x41, 0,0,0) #A
+        win32gui.PostMessage(window.id, wcon.WM_KEYDOWN, 0x41, 0)
+        win32gui.PostMessage(window.id, wcon.WM_KEYDOWN, 0x41, 0)
+        # win32api.keybd_event(0x41,0 ,wcon.KEYEVENTF_KEYUP ,0)
+        win32gui.PostMessage(window.id, wcon.WM_LBUTTONUP,
+                                wcon.MK_LBUTTON, lParam)
+        time.sleep(0.5)
+
+    def d_click(self, x, y):
+        x += window.x
+        y += window.y
+        lParam = win32api.MAKELONG(x, y)
+        # MOUSEMOVE event is required for game to register clicks correctly
+        win32gui.PostMessage(window.id, wcon.WM_MOUSEMOVE, 0, lParam)
+        #Left click then D
+        win32gui.PostMessage(window.id, wcon.WM_LBUTTONDOWN,
+                                wcon.MK_LBUTTON, lParam)
+        # win32api.keybd_event(0x44, 0,0,0) #D
+        win32gui.PostMessage(window.id, wcon.WM_KEYDOWN, 0x44, 0)
+        win32gui.PostMessage(window.id, wcon.WM_KEYDOWN, 0x44, 0)
+        # win32api.keybd_event(0x44,0 ,wcon.KEYEVENTF_KEYUP ,0)
+        win32gui.PostMessage(window.id, wcon.WM_LBUTTONUP,
+                                wcon.MK_LBUTTON, lParam)
+        time.sleep(0.5)
