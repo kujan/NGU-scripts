4083f31d0a315fe8bc361c22a8ff27040ee09400
Jathan Haggard <jvhaggard@gmail.com>
2019-02-03 14:45:11 +0000

    5min script changes

--- Python/Scripts/5min.py
+++ Python/Scripts/5min.py
@@ -1,13 +1,10 @@
 """5-minute rebirth script"""
 
-# Challenges
-from challenges.basic import Basic
-from challenges.level import Level
-
 # Helper classes
-from classes.challenge import Challenge
 from classes.features import Features
+from classes.alt_features import AltFeatures
 from classes.inputs import Inputs
+from classes.alt_inputs import AltInputs
 from classes.navigation import Navigation
 from classes.stats import Stats, EstimateRate, Tracker
 from classes.upgrade import Upgrade
@@ -17,7 +14,7 @@ import ngucon as ncon
 import time
 
 
-def speedrun(duration, f):
+def speedrun(duration, f, af):
     """Start a speedrun.
     Keyword arguments
     duration -- duration in minutes to run
@@ -29,21 +26,22 @@ def speedrun(duration, f):
     blood_digger_active = False
     itopod_advance = False
     f.nuke()
+    time.sleep(2)
     f.loadout(2)  # Gold drop equipment
     f.adventure(highest=True)
-    time.sleep(5)
+    time.sleep(3)
     f.loadout(1)  # Bar/power equimpent
     f.adventure(itopod=True, itopodauto=True)
     #Sleep so can get enough E/M for augments
     time.sleep(5)
-    f.augments({"MI": 0.3, "DTMT": 0.7}, 2e7) #20m
+    # f.augments({"CI": 0.3, "ML": 0.7}, 1e8) #100m
+    f.augments({"SM": 0.3, "AA": 0.7}, 1e8) #100m
     #Sleep so time machine can get some E/M
     #TODO: Optimize
-    time.sleep(15)
-    f.blood_magic(4)
+    time.sleep(10)
+    f.blood_magic(5)
     f.time_machine(True)
-    f.boost_equipment()
-    f.boost_cube()
+    af.boost_cube()
     f.gold_diggers([2, 3, 5, 6], True)
     f.wandoos(True)
     #Add turning on diggers here
@@ -57,12 +55,12 @@ def speedrun(duration, f):
         #     f.gold_diggers([11], True)
 
         #Don't have enough to mess with NGU in short runs yet
-        if time.time () > start + 120:
+        if time.time () > start + 90:
             try:
                 NGU_energy = int(f.remove_letters(f.ocr(ncon.OCR_ENERGY_X1, ncon.OCR_ENERGY_Y1, ncon.OCR_ENERGY_X2, ncon.OCR_ENERGY_Y2)))
-                feature.assign_ngu(NGU_energy, [1, 2, 5, 6])
+                feature.assign_ngu(NGU_energy, [6])
                 NGU_magic = int(f.remove_letters(f.ocr(ncon.OCR_MAGIC_X1, ncon.OCR_MAGIC_Y1, ncon.OCR_MAGIC_X2, ncon.OCR_MAGIC_Y2)))
-                feature.assign_ngu(NGU_magic, [2, 3], magic=True)
+                feature.assign_ngu(NGU_magic, [3], magic=True)
             except ValueError:
                 print("couldn't assign e/m to NGUs")
             time.sleep(0.5)
@@ -70,13 +68,6 @@ def speedrun(duration, f):
         if time.time() > start + 90 and not itopod_advance:
             f.adventure(itopod=True, itopodauto=True)
             itopod_advance = True
-
-        # Do some early fights to open up furthest area for gold
-        if time.time() < end - (duration * 0.75 * 60):
-            #Make sure we're making maximum gold as the run progresses
-            f.adventure(highest=True)
-            time.sleep(7)
-            f.adventure(itopod=True, itopodauto=True)
 
         # if time.time() > start + 120 and not blood_digger_active:
         #     blood_digger_active = True
@@ -103,8 +94,10 @@ def speedrun(duration, f):
 
 w = Window()
 i = Inputs()
+ai = AltInputs()
 nav = Navigation()
 feature = Features()
+alt_feature = AltFeatures()
 
 Window.x, Window.y = i.pixel_search(ncon.TOP_LEFT_COLOR, 0, 0, 400, 600)
 nav.menu("inventory")
@@ -115,9 +108,9 @@ print(w.x, w.y)
 tracker = Tracker(5)
 count = 0
 
-# while count <= 11: # Adjust # for how many runs you want to do during testing minus 1
-#     count += 1
-#     speedrun(5, feature)
-
+# while count <= 0: # Adjust # for how many runs you want to do during testing minus 1
+    # count += 1
+    # speedrun(5, feature, alt_feature)
+#
 while True:  # main loop to go foreverrrrr
-    speedrun(5, feature)
+    speedrun(5, feature, alt_feature)
