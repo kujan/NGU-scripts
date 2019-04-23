cb9df597c569ec75b4e9beb9199796e015d22d3e
Fredrik Kujansuu <fredrik.k91@gmail.com>
2019-04-23 19:10:34 +0200

    Update window.py
    set dpi awareness for people using window scaling

--- Python/Scripts/classes/window.py
+++ Python/Scripts/classes/window.py
@@ -1,6 +1,6 @@
 """Window class contains the coordinate for the top left of the game window."""
 import win32gui
-
+import ctypes
 
 class Window():
     """This class contains game window coordinates."""
@@ -17,6 +17,7 @@ class Window():
         x -- The x-coordinate for the top left corner of the game window.
         y -- The y-coordinate for the top left corner of the game window.
         """
+        ctypes.windll.shcore.SetProcessDpiAwareness(2)
         def window_enumeration_handler(hwnd, top_windows):
             """Add window title and ID to array."""
             top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
