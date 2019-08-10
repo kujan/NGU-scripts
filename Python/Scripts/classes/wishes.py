fbbe647d458d447b3cee29faed580df31fd5d11a
Fredrik Kujansuu <fredrik.k91@gmail.com>
2019-08-10 23:43:28 +0200

    wish allocation

--- Python/Scripts/classes/wishes.py
+++ Python/Scripts/classes/wishes.py
@@ -1,15 +1,13 @@
-from classes.inputs import Inputs
-from classes.navigation import Navigation
-from classes.window import Window
+from classes.features import Features
 import constants as const
 import coordinates as coords
+from functools import reduce
 import math
 import re
-import time
-import usersettings as userset
+import pprint
 
 
-class Wishes(Navigation, Inputs):
+class Wishes(Features):
     """Class that handles wishes."""
 
     def __init__(self, wish_slots):
@@ -24,16 +22,15 @@ class Wishes(Navigation, Inputs):
 
         self.rpow = 0
         self.rcap = 0
-
+        self.pp = pprint.PrettyPrinter(indent=4)
         self.wishes_completed = []  # completed wishes
         self.wishes_in_progress = []  # wishes above level 0
         self.wishes_active = []  # wishes that currently are progressing
-        self.wishes_inactive = []  # wishes that have resources but are not progressing
-        #self.get_breakdowns()
-        self.get_wish_status()
-        print(self.wishes_completed)
-        print(self.wishes_in_progress)
-        print(self.wishes_active)
+        self.get_breakdowns()
+        self.allocate_wishes()
+        #self.get_wish_status()
+        #self.allocate_wishes()
+        #print(self.wish_speed)
 
     def get_breakdowns(self):
         """Go to stat breakdowns and fetch the necessary stats."""
@@ -43,21 +40,45 @@ class Wishes(Navigation, Inputs):
         self.click(*coords.BREAKDOWN_M)
         m_list = self.fix_text(self.ocr(*coords.OCR_BREAKDOWN_E))
         self.click(*coords.BREAKDOWN_R)
-        r_list = self.fix_text(self.ocr(*coords.OCR_BREAKDOWN_E, debug=True))
+        r_list = self.fix_text(self.ocr(*coords.OCR_BREAKDOWN_E))
+        self.click(*coords.BREAKDOWN_MISC)
+        misc_list = self.fix_text(self.ocr(*coords.OCR_BREAKDOWN_E))
 
-        fields = ["total energy power:", "total magic power:", "total r power:"]
+        fields = ["total energy power:", "total magic power:", "total r power:", "total wish speed:"]
 
-        for e in e_list:
-            print(e[0])
-            if e[0].lower() in fields:
-                self.epow = e[1]
-                print(e[1])
-        for e in m_list:
-            if e[0].lower() in fields:
-                self.mpow = e[1]
-        for e in r_list:
-            if e[0].lower() in fields:
-                self.rpow = e[1]
+        try:
+            for e in e_list:
+                if e[0].lower() in fields:
+                    self.epow = float(e[1])
+        except ValueError:
+            print("couldn't fetch energy power")
+            self.epow = 0
+        try:
+            for e in m_list:
+                if e[0].lower() in fields:
+                    self.mpow = float(e[1])
+        except ValueError:
+            print("couldn't fetch magic power")
+            self.mpow = 0
+
+        try:
+            for e in r_list:
+                if e[0].lower() in fields:
+                    self.rpow = float(e[1])
+        except ValueError:
+            print("couldn't fetch R3 power")
+            self.rpow = 0
+        try:
+            for e in misc_list:
+                if e[0].lower() in fields:
+                    self.wish_speed = int(e[1]) / 100
+        except ValueError:
+            print("Couldn't fetch wish speed, defaulting to 100%")
+            self.wish_speed = 1
+
+        self.ecap = self.get_idle_cap(1)
+        self.mcap = self.get_idle_cap(2)
+        self.rcap = self.get_idle_cap(3)
 
     def fix_text(self, text):
         """Fix OCR output to something useable."""
@@ -69,7 +90,7 @@ class Wishes(Navigation, Inputs):
                 if line == "" or line[0].lower() == "x":
                     continue
                 if line[0].isdigit():
-                    values.append(re.sub(r'[^0-9%E+\.]', '', line))
+                    values.append(re.sub(r'[^0-9E+\.]', '', line))
                 else:
                     fields.append(line)
             assert(len(fields) == len(values))
@@ -133,5 +154,22 @@ class Wishes(Navigation, Inputs):
 
     def allocate_wishes(self):
         """Use the order defined in constants.py to determine which wish to run."""
-        for _ in range(self.wish_slots):
-            print("")
+        available_wishes = const.WISH_ORDER
+        costs = {}
+        for wish in available_wishes:
+            if wish.id in self.wishes_completed:
+                available_wishes.remove(wish)
+        # TODO: check wish level
+        for wish in available_wishes:
+            powproduct = (self.epow * self.mpow * self.rpow) ** 0.17
+            wish_cap_ticks = 218 * 60 * 50
+            capreq = wish.divider * 2 / wish_cap_ticks / self.wish_speed / powproduct
+
+            ratio = [self.ecap / self.rcap, self.mcap / self.rcap, 1]
+            capproduct = reduce((lambda x, y: x * y), ratio)
+            factor = (capreq / capproduct ** 0.17) ** (1 / .17 / 3)
+            vals = []
+            for x in ratio:
+                vals.append(math.ceil((x * factor)))
+            costs[wish.id] = vals
+        self.pp.pprint(costs)
