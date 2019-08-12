ba36e20ca3b8f192a4a3193d2690fda7e95a3755
Fredrik Kujansuu <fredrik.k91@gmail.com>
2019-08-12 19:25:56 +0200

    rename guffin2.py

--- Python/Scripts/guffin.py
+++ Python/Scripts/guffin.py
@@ -3,98 +3,139 @@
 # Helper classes
 from classes.features import Features
 from classes.window import Window
+from classes.wishes import Wishes
 
 import coordinates as coords
-import datetime
 import time
 
 
-def speedrun(f):
-    """Procedure that handles start of rebirth."""
-    end = time.time() + 30 * 60
-    rt = f.get_rebirth_time()
-    toggle_number = False
-    f.nuke()  # PPP
-    time.sleep(2)
-    f.loadout(7)  # gold
-    f.adventure(highest=True)
-    f.toggle_auto_spells(number=False, drop=False)
-    f.gold_diggers([x for x in range(1, 13)])
-    f.blood_magic(8)
-    f.set_ngu_overcap(102)
-    if feature.check_pixel_color(*coords.NGU_EVIL):
-        f.click(778, 122)
-    f.cap_ngu()
-    f.cap_ngu(magic=True)
-    f.set_wandoos(0)
-    f.wandoos(True)
-    f.augments({"CI": 0.66, "ML": 0.34}, f.get_idle_cap(1) * 0.5)
-    f.time_machine(coords.INPUT_MAX, magic=True)
-    f.loadout(3)
+class Guffin(Features):
+    """Guffin run class."""
 
-    while rt.timestamp.tm_hour < 1 and rt.timestamp.tm_min < 13:
-        if rt.timestamp.tm_min > 1 and not toggle_number:
-            f.toggle_auto_spells(drop=False, gold=False)
-            toggle_number = True
-        text = f.get_quest_text()
-        majors = f.get_available_majors()
-        if majors == 0 and (coords.QUESTING_MINOR_QUEST in text.lower() or coords.QUESTING_NO_QUEST_ACTIVE in text.lower()):
-            f.questing(duration=2, force=3)
+    def __init__(self, wish_slots, wish_min_time):
+        """Get breakdowns for wishes."""
+        ##############
+        # EDIT BELOW #
+        ##############
+        self._max_rb_duration = 1800  # How long in seconds you want to run
+        self._zone = 3  # The zone number in which you want to do minor quests (for farming specific guffs)
+        self._hacks = [2, 9, 11, 13, 14]  # Adv, QP, Exp, PP, Hack
+        self._diggers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]  # All
+        self._butter = True  # Butter majors? True/False
+        self._aug = ["EB", "CS"]  # Which aug/upgrade to use, see naming convention in augments() in features.py
+        #####################
+        # DO NOT EDIT BELOW #
+        #####################
+        print("If you want to use muffins, use them manually. You can eat several muffins at once to extend the duration above 24 hours.")
+        input("Press enter to rebirth and start script. ")
+        self._wishes = Wishes(wish_slots, wish_min_time)
+        lst = [self._wishes.epow, self._wishes.mpow, self._wishes.rpow]
+        i = 0
+        while 1 in lst:
+            print("OCR reading failed for stat breakdowns, trying again...")
+            self.wishes = Wishes()
+            i += 1
+            if i > 5:
+                print("Wishes will be disabled.")
+                self.wishes = None
+                break
+
+        self.runs = 0
+        self.do_rebirth()
+        self.run()
+
+    def __update_gamestate(self):
+        """Update relevant state information."""
+        self._rb_time = self.rt_to_seconds()
+        self._advanced_training_locked = True
+        try:
+            self._current_boss = int(self.get_current_boss())
+        except ValueError:
+            self._current_boss = 1
+            print("couldn't get current boss")
+
+        if self._advanced_training_locked:
+            self._advanced_training_locked = self.check_pixel_color(*coords.COLOR_ADV_TRAINING_LOCKED)
+
+    def __do_quest(self):
+        """Get the amount of available major quests."""
+        text = self.get_quest_text().lower()
+        majors = self.get_available_majors()
+        if majors == 0 and (coords.QUESTING_MINOR_QUEST in text or coords.QUESTING_NO_QUEST_ACTIVE in text):
+            self.questing(duration=2, force=self._zone)
         else:
-            if not f.check_pixel_color(*coords.COLOR_QUESTING_USE_MAJOR):
-                f.click(*coords.QUESTING_USE_MAJOR)
-            f.questing(duration=2)
-        f.nuke()
-        f.gold_diggers([x for x in range(1, 13)])
-        f.time_machine(coords.INPUT_MAX, magic=True)
-        f.augments({"CI": 0.66, "ML": 0.34}, f.get_idle_cap(1) * 0.5)
-        f.time_machine(coords.INPUT_MAX, magic=True)
-        rt = f.get_rebirth_time()
+            if not self.check_pixel_color(*coords.COLOR_QUESTING_USE_MAJOR):
+                self.click(*coords.QUESTING_USE_MAJOR)
+            self.questing(duration=2, butter=self._butter)
 
-    f.adventure(highest=True)
-    f.reclaim_tm(True)
-    f.reclaim_aug()
-    f.advanced_training(2e12)
-    f.set_wandoos(1)
-    f.wandoos(True)
-    f.augments({"CI": 0.66, "ML": 0.34}, f.get_idle_cap(1) * 0.5)
-    f.time_machine(coords.INPUT_MAX, magic=True)
+    def run(self):
+        """Rebirth procedure"""
+        self.__update_gamestate()
+        self.nuke()
+        time.sleep(2)
+        self.adventure(highest=True)
+        self.toggle_auto_spells(number=False, drop=False)
+        self.gold_diggers(self._diggers)
+        self.blood_magic(8)
+        self.cap_ngu()
+        self.cap_ngu(magic=True)
+        self.set_wandoos(0)
+        self.wandoos(True)
+        self.augments({self._aug[0]: 0.66, self._aug[1]: 0.34}, self.get_idle_cap(1) * 0.5)
+        self.time_machine(self.get_idle_cap(1) * 0.1, magic=True)
+        self.__update_gamestate()
+        self.toggle_auto_spells(drop=False, gold=False)
+        self.hacks(self._hacks, self.get_idle_cap(3))
+        if self._wishes:
+            self._wishes.get_caps()
+            self._wishes.allocate_wishes()
 
-    while time.time() < end - 140:
-        f.gold_diggers([x for x in range(1, 13)])
-        f.nuke()
-        rt = f.get_rebirth_time()
-        text = f.get_quest_text()
-        majors = f.get_available_majors()
-        if majors == 0 and (coords.QUESTING_MINOR_QUEST in text.lower() or coords.QUESTING_NO_QUEST_ACTIVE in text.lower()):
-            f.questing(duration=2, force=3)
-        else:
-            if not f.check_pixel_color(*coords.COLOR_QUESTING_USE_MAJOR):
-                f.click(*coords.QUESTING_USE_MAJOR)
-            f.questing(duration=2)
-    f.nuke()
-    f.fight()
-    f.adventure(itopodauto=True)
-    f.pit()
-    f.save_check()
-    while time.time() < end + 2:
-        time.sleep(1)
-    f.nuke()
-    f.do_rebirth()
+        while self._advanced_training_locked:
+            self.__do_quest()
+            self.nuke()
+            self.gold_diggers(self._diggers)
+            self.cap_ngu()
+            self.cap_ngu(magic=True)
+            self.hacks(self._hacks, self.get_idle_cap(3))
+            self.augments({self._aug[0]: 0.66, self._aug[1]: 0.34}, self.get_idle_cap(1) * 0.5)
+            self.time_machine(coords.INPUT_MAX, magic=True)
+            self.__update_gamestate()
+
+        self.adventure(highest=True)
+        self.reclaim_tm(True)
+        self.reclaim_aug()
+        self.advanced_training(2e12)
+        self.set_wandoos(1)
+        self.wandoos(True)
+        self.augments({self._aug[0]: 0.66, self._aug[1]: 0.34}, self.get_idle_cap(1) * 0.5)
+
+        while self._rb_time < self._max_rb_duration - 140:
+            self.gold_diggers(self._diggers)
+            self.nuke()
+            self.hacks(self._hacks, self.get_idle_cap(3))
+            self.__do_quest()
+            self.__update_gamestate()
+
+        self.nuke()
+        self.fight()
+        self.adventure(itopodauto=True)
+        self.pit()
+        self.save_check()
+        while self._rb_time < self._max_rb_duration:
+            time.sleep(1)
+            self.__update_gamestate()
+
+        self.do_rebirth()
+        self.runs += 1
+        time.strftime('%H:%M:%S', time.gmtime(12345))
+        print(f"Completed guffin run #{self.runs} in {time.strftime('%H:%M:%S', time.gmtime(self._rb_time))}")
 
 
-w = Window()
+w = Window(True)
 feature = Features()
-
 Window.x, Window.y = feature.pixel_search(coords.TOP_LEFT_COLOR, 0, 0, 400, 600)
 feature.menu("inventory")
-print(f"Top left found at: {w.x}, {w.y}")
+guffin = Guffin(4, 218)
 
-print("If you want to use muffins, use them manually. You can eat several muffins at once to extend the duration above 24 hours.")
-input("Press enter to rebirth and start script. ")
-feature.do_rebirth()
-runs = 1
 while True:
-    speedrun(feature)
-    print(f"Completed guffin run #{runs} at {datetime.datetime.now().strftime('%H:%M:%S')}")
-    runs += 1
+    guffin.run()
