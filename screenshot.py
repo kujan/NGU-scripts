65894a09fe33d111ff4fb52aee9b8b306db15e89
DavidFerrerBeltran <ferrerdavid96@gmail.com>
2019-10-14 14:30:56 +0200

    replacing Window() from code

--- screenshot.py
+++ screenshot.py
@@ -2,7 +2,7 @@ from classes.inputs import Inputs
 from classes.window import Window
 import coordinates as coords
 
-w = Window()
+Window.init()
 i = Inputs()
 
 Window.x, Window.y = i.pixel_search(coords.TOP_LEFT_COLOR, 0, 0, 400, 600)
