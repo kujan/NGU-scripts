5ac9702192a716cc6d12ae5b90baaf35207b6203
DavidFerrerBeltran <ferrerdavid96@gmail.com>
2019-10-14 11:00:25 +0200

    Update usersettings_example.py
    solving #24 for usersettings

--- usersettings_example.py
+++ usersettings_example.py
@@ -7,10 +7,11 @@ MEDIUM_SLEEP = 0.3
 LONG_SLEEP = 0.4
 
 # How long to farm blood for spell casting (in seconds)
-
 SPELL = 300
 
 # DISCORD WEBHOOK URLS
-
 ERROR_URL = ""
 INFO_URL = ""
+
+# STATS
+E_RATE_KEEP_RUNS = 60
