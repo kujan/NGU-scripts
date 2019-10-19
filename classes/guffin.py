c1697ceee5dd9e5dec6ee37cf7c49aeb72d27244
David Ferrer Beltran <ferrerdavid96@gmail.com>
2019-10-19 17:22:08 +0200

    Adjusted new class Guffin
    It should now work well

--- classes/guffin.py
+++ classes/guffin.py
@@ -49,49 +49,47 @@ class Guffin():
                     break
 
         self.runs = 0
-        self.do_rebirth()
-        self.run()
 
     def __update_gamestate(self):
         """Update relevant state information."""
-        self._rb_time = self.rt_to_seconds()
+        self._rb_time = Rebirth.rt_to_seconds()
         try:
-            self._current_boss = int(self.get_current_boss())
+            self._current_boss = int(FightBoss.get_current_boss())
         except ValueError:
             self._current_boss = 1
             print("couldn't get current boss")
 
         if self._advanced_training_locked:
-            self._advanced_training_locked = self.check_pixel_color(*coords.COLOR_ADV_TRAINING_LOCKED)
+            self._advanced_training_locked = Inputs.check_pixel_color(*coords.COLOR_ADV_TRAINING_LOCKED)
 
     def __do_quest(self):
         """Get the amount of available major quests."""
-        text = self.get_quest_text().lower()
-        majors = self.get_available_majors()
+        text = Questing.get_quest_text().lower()
+        majors = Questing.get_available_majors()
         if majors == 0 and (coords.QUESTING_MINOR_QUEST in text or coords.QUESTING_NO_QUEST_ACTIVE in text):
-            self.questing(duration=2, force=self._zone)
+            Questing.questing(duration=2, force=self._zone)
         else:
-            if not self.check_pixel_color(*coords.COLOR_QUESTING_USE_MAJOR):
+            if not Inputs.check_pixel_color(*coords.COLOR_QUESTING_USE_MAJOR):
                 Inputs.click(*coords.QUESTING_USE_MAJOR)
-            self.questing(duration=2, butter=self._butter)
+            Questing.questing(duration=2, butter=self._butter)
 
     def run(self):
         """Rebirth procedure."""
         self.__update_gamestate()
-        self.nuke()
+        FightBoss.nuke()
         time.sleep(2)
-        self.adventure(highest=True)
-        self.toggle_auto_spells(number=False, drop=False)
-        self.gold_diggers(self._diggers)
-        self.blood_magic(8)
-        self.cap_ngu()
-        self.cap_ngu(magic=True)
-        self.set_wandoos(0)
-        self.wandoos(True)
-        self.augments({self._aug[0]: 0.66, self._aug[1]: 0.34}, self.get_idle_cap(1) * 0.5)
-        self.time_machine(self.get_idle_cap(1) * 0.1, magic=True)
+        Adventure.adventure(highest=True)
+        BloodMagic.toggle_auto_spells(number=False, drop=False)
+        GoldDiggers.gold_diggers(self._diggers)
+        BloodMagic.blood_magic(8)
+        NGU.cap_ngu()
+        NGU.cap_ngu(magic=True)
+        Wandoos.set_wandoos(0)
+        Wandoos.wandoos(True)
+        Augmentation.augments({self._aug[0]: 0.66, self._aug[1]: 0.34}, self.get_idle_cap(1) * 0.5)
+        TimeMachine.time_machine(self.get_idle_cap(1) * 0.1, magic=True)
         self.__update_gamestate()
-        self.toggle_auto_spells(drop=False, gold=False)
+        BloodMagic.toggle_auto_spells(drop=False, gold=False)
         if self._wishes:
             self._wishes.get_caps()
             self._wishes.get_wish_status()
@@ -99,40 +97,40 @@ class Guffin():
 
         while self._advanced_training_locked:
             self.__do_quest()
-            self.nuke()
-            self.gold_diggers(self._diggers)
-            self.cap_ngu()
-            self.cap_ngu(magic=True)
-            self.hacks(self._hacks, coords.INPUT_MAX)
-            self.augments({self._aug[0]: 0.66, self._aug[1]: 0.34}, self.get_idle_cap(1) * 0.5)
-            self.time_machine(coords.INPUT_MAX, magic=True)
+            FightBoss.nuke()
+            GoldDiggers.gold_diggers(self._diggers)
+            NGU.cap_ngu()
+            NGU.cap_ngu(magic=True)
+            Hacks.hacks(self._hacks, coords.INPUT_MAX)
+            Augmentation.augments({self._aug[0]: 0.66, self._aug[1]: 0.34}, self.get_idle_cap(1) * 0.5)
+            TimeMachine.time_machine(coords.INPUT_MAX, magic=True)
             self.__update_gamestate()
 
-        self.adventure(highest=True)
-        self.reclaim_tm(True)
-        self.reclaim_aug()
-        self.advanced_training(2e12)
-        self.set_wandoos(1)
-        self.wandoos(True)
-        self.augments({self._aug[0]: 0.66, self._aug[1]: 0.34}, self.get_idle_cap(1) * 0.5)
+        Adventure.adventure(highest=True)
+        Misc.reclaim_tm(energy=True, magic=True)
+        Misc.reclaim_aug()
+        AdvancedTraining.advanced_training(2e12)
+        Wandoos.set_wandoos(1)
+        Wandoos.wandoos(True)
+        Augmentation.augments({self._aug[0]: 0.66, self._aug[1]: 0.34}, self.get_idle_cap(1) * 0.5)
 
         while self._rb_time < self._max_rb_duration - 140:
-            self.gold_diggers(self._diggers)
-            self.nuke()
-            self.hacks(self._hacks, coords.INPUT_MAX)
+            GoldDiggers.gold_diggers(self._diggers)
+            FightBoss.nuke()
+            Hacks.hacks(self._hacks, coords.INPUT_MAX)
             self.__do_quest()
             self.__update_gamestate()
 
-        self.fight()
-        self.adventure(itopodauto=True)
-        self.pit()
-        self.save_check()
+        FightBoss.fight()
+        Adventure.adventure(itopodauto=True)
+        MoneyPit.pit()
+        Misc.save_check()
         while self._rb_time < self._max_rb_duration:
             time.sleep(1)
             self.__update_gamestate()
 
-        self.nuke()
-        self.do_rebirth()
+        FightBoss.nuke()
+        Rebirth.do_rebirth()
         self.runs += 1
         time.strftime('%H:%M:%S', time.gmtime(12345))
         print(f"Completed guffin run #{self.runs} in {time.strftime('%H:%M:%S', time.gmtime(self._rb_time))}")
