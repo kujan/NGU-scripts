d8212c4c70b9420d481daa9045db90fbf30100c9
DavidFerrerBeltran <ferrerdavid96@gmail.com>
2019-10-25 13:44:02 +0200

    Now using inspect.signature
    #7

--- i.py
+++ i.py
@@ -50,38 +50,9 @@ def showFuncs(cls=None):
   
 # Show arguments of a function or method  
 def showArgs(func):
-    fas = inspect.getfullargspec(func)
-    
-    if fas.args != []:
-        print("Arguments:")
-        if fas.defaults is None:
-            for arg in fas.args: print(f"\t{arg}")
-        else:
-            la = len(fas.args)
-            ld = len(fas.defaults)
-            args1 = fas.args[:-ld]
-            args2 = fas.args[-ld:]
-            for arg in args1: print(f"\t{arg}")
-            for tup in zip(args2, fas.defaults):
-                print(f"\t{tup[0]} = {tup[1]}")
-    
-    if fas.varargs is not None:
-        print("Variable Arguments:")
-        for arg in fas.varargs: print(f"\t{arg}")
-    
-    if fas.varkw is not None:
-        print("Variable Keyword Arguments:")
-        for arg in fas.varkw: print(f"\t{arg}")
-    
-    if fas.kwonlyargs != []:
-        print("Keyword-only Arguments:")
-        if fas.kwonlydefaults is not None:
-            for arg in fas.kwonlyargs:
-                if arg in fas.kwonlydefaults:
-                    print(f"\t{arg} = {fas.kwonlydefaults[arg]}")
-                else: print(f"\t{arg}")
-        else:
-            for arg in fas.kwonlyargs: print(f"\t{arg}")
+    print(f"From module {inspect.getmodule(func)}")
+    print(inspect.signature(func))
+    print(inspect.getdoc(func))
 
 print("Imported the Interactive Scripting Helper.")
 print("This is meant to be used ONLY on an interactive Python session.")
