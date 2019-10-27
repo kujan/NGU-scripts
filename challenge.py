764146e3df6449ef9480b9c9e7f674be34550fbc
David Ferrer Beltran <ferrerdavid96@gmail.com>
2019-10-27 02:28:27 +0200

    Updated challenge.py script
    Now it doesn't force the -t argument, and defaults it to 1
    
    Made so that argument parsing takes place before Helper.init() so it
    doesn't waste time trying to init if it gets wrong arguments
    
    Main loop now prints a message when it starts

--- challenge.py
+++ challenge.py
@@ -7,19 +7,20 @@ from classes.challenge import Challenge
 
 import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--challenge", required=True, type=int, help="select which challenge you wish to run (1-11)")
+parser.add_argument("-t", "--times", default=1, type=int, help="number of times to run challenge")
+args = parser.parse_args()
+
 Helper.init(True)
 Helper.requirements()
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--challenge", required=True, type=int, help="select which challenge you wish to run (1-11")
-parser.add_argument("-t", "--times", required=True, type=int, help="number of times to run challenge")
-args = parser.parse_args()
 
 print(f"Running challenge #{args.challenge} {args.times} times")
 for x in range(args.times):
     Challenge.start_challenge(args.challenge)
 
-
+print("Finished doing all challenges")
+print("Engaging idling loop")
 while True:  # main loop
     Adventure.itopod_snipe(300)
     MoneyPit.pit()
