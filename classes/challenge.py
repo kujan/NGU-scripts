b659049dec58042970cc7974bd085d2be2c17249
David Ferrer Beltran <ferrerdavid96@gmail.com>
2019-11-05 20:31:52 +0100

    Improved classes.challenge
    Now the challenges are stored in a list, which simplifies some things

--- classes/challenge.py
+++ classes/challenge.py
@@ -11,9 +11,13 @@ import usersettings as userset
 import time
 
 
-class Challenge:
-    """Handles different challenges."""
+class cInfo:
+    def __init__(self, name = "", script = None, extra = []):
+        self.name = name
+        self.script = script
+        self.extra = extra
 
+def init(ChList):
     from .challenges.augment     import augment
     from .challenges.basic       import basic
     from .challenges.blind       import blind
@@ -24,8 +28,48 @@ class Challenge:
     from .challenges.rebirth     import rebirth
     from .challenges.timemachine import timemachine
 
+    def get24boss():
+        try:
+            x = coords.CHALLENGE.x
+            y = coords.CHALLENGE.y + 3 * coords.CHALLENGEOFFSET
+            
+            Navigation.challenges()
+            Inputs.click(x, y, button="right")
+            time.sleep(userset.LONG_SLEEP)
+            target = Inputs.ocr(*coords.OCR_CHALLENGE_24HC_TARGET)
+            target = Inputs.get_numbers(target)[0]
+            return f"Target boss: {target}"
+        except ValueError:
+            Discord.send_message("Couldn't detect the target level of 24HC", Discord.ERROR)
+            return "Couldn't detect the target level of 24HC"
+
+    ChList.append(cInfo("Basic", basic))
+    ChList.append(cInfo("No Augments", augment))
+    ChList.append(cInfo("24h", basic, [get24boss]))
+    ChList.append(cInfo("100 Level", level, [
+        "IMPORTANT",
+        "Set target level for energy buster to 67 and charge shot to 33.",
+        "Disable 'Advance Energy' in Augmentation",
+        "Disable beards if you cap ultra fast."
+    ]))
+    ChList.append(cInfo("No Equipment", equipment))
+    ChList.append(cInfo("Troll", Window.shake, ["Do it yourself, fam."]))
+    ChList.append(cInfo("No Rebirth", rebirth))
+    ChList.append(cInfo("Laser Sword", laser, [
+        "LSC doesn't reset your number, make sure your number is high enough to make laser swords."
+    ]))
+    ChList.append(cInfo("Blind", blind))
+    ChList.append(cInfo("No NGU", ngu))
+    ChList.append(cInfo("No Time Machine", timemachine))
+    
+ChList = []
+init(ChList)
+   
+class Challenge:
+    """Handles different challenges."""
+    
     @staticmethod
-    def run_challenge(challenge :int, cont :bool =False) -> None:
+    def run_challenge(challenge :int, cont :bool =False) -> None:        
         """Run the selected challenge.
         
         Keyword arguments
@@ -33,89 +77,24 @@ class Challenge:
                      ending at 11 for No TM challenge
         cont      -- Whether the challenge is already running.
         """
-        x = coords.CHALLENGE.x
-        y = coords.CHALLENGE.y + challenge * coords.CHALLENGEOFFSET
-        post_msg = []
-
-        if   challenge ==  1: # Basic
-            cname = "Basic"
-            run_script = Challenge.basic
-
-        elif challenge ==  2: # No Augments
-            cname = "No Augments"
-            run_script = Challenge.augment
-
-        elif challenge ==  3: # 24 h
-            cname = "24h"
-            try:
-                Inputs.click(x, y, button="right")
-                time.sleep(userset.LONG_SLEEP)
-                target = Inputs.ocr(*coords.OCR_CHALLENGE_24HC_TARGET)
-                target = Inputs.get_numbers(target)[0]
-                post_msg.append(f"Found target boss: {target}")
-                run_script = Challenge.basic
-
-            except ValueError:
-                print("Couldn't detect the target level of 24HC")
-                Discord.send_message("Couldn't detect the target level of 24HC", Discord.ERROR)
-                return
-
-        elif challenge ==  4: # 100 lvl
-            cname = "100 Level"
-            post_msg.append("IMPORTANT")
-            post_msg.append("Set target level for energy buster to 67 and charge shot to 33.")
-            post_msg.append("Disable 'Advance Energy' in Augmentation")
-            post_msg.append("Disable beards if you cap ultra fast.")
-            run_script = Challenge.level
-
-        elif challenge ==  5: # No Equipment
-            cname = "No Equipment"
-            run_script = Challenge.equipment
-
-        elif challenge ==  6: # TROLL
-            # cname = "The Troll"
-            print("Troll Challenge, really?")
-            print("Nah fam. Do it yourself")
-            Window.shake()
-            return
-
-        elif challenge ==  7: # No Rebirth
-            cname = "No Rebirth"
-            run_script = Challenge.rebirth
-
-        elif challenge ==  8: # Laser
-            cname = "Laser Sword"
-            post_msg.append("LSC doesn't reset your number, make sure your number is high enough to make laser swords.")
-            run_script = Challenge.laser
-
-        elif challenge ==  9: # Blind
-            cname = "Blind"
-            run_script = Challenge.blind
-
-        elif challenge == 10: # No NGU
-            cname = "No NGU"
-            run_script = Challenge.ngu
-
-        elif challenge == 11: # No TM
-            cname = "No TM"
-            run_script = Challenge.timemachine
-
-        else: # Wrong Challenge
-            print(f"Challenge {challenge} doesn't exist.")
-            return
-
+        global ChList
+        Navigation.challenges()
+        
         if cont: pass
         else:
+            x = coords.CHALLENGE.x
+            y = coords.CHALLENGE.y + challenge * coords.CHALLENGEOFFSET
             Inputs.click(x, y)
             time.sleep(userset.LONG_SLEEP)
             Navigation.confirm()
-
-        print(f"Starting {cname} Challenge script.")
-        if post_msg != []:
-            print(*post_msg)
-
-        run_script()
-
+        
+        chall = ChList[challenge-1]
+        print(f"Starting {chall.name} Challenge script.")
+        for x in chall.extra:
+            if callable(x): print(x())
+            else: print(x)
+        chall.script()
+    
     @staticmethod
     def start_challenge(challenge :int, quitCurrent :bool =False) -> None:
         """Start the selected challenge. Checks for currently running challenges.
@@ -126,8 +105,7 @@ class Challenge:
         quitCurrent -- Quit the current challenge if it is different to the desired.
         """
         BloodMagic.toggle_auto_spells(drop=False)
-        Navigation.challenges()
-
+        
         chall = Rebirth.check_challenge(getNum=True)
         if chall and chall != challenge:
             print(f"A challenge is currently running ({chall}).")
@@ -137,3 +115,13 @@ class Challenge:
             else: Challenge.run_challenge(chall, cont=True)
 
         Challenge.run_challenge(challenge)
+    
+    @staticmethod
+    def list() -> None:
+        """Print the list of challenges with their corresponding number.
+        """
+        global ChList
+        for i in range(len(ChList)):
+            if i < 9: print(i+1, "  " + ChList[i].name)
+            else:     print(i+1,  " " + ChList[i].name)
+    
