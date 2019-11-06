8e5597992d4b2b8a8170a94494399143b80a1852
Jasper Borsboom <jasper.borsboom@gmail.com>
2019-11-06 17:43:27 +0100

    remove virtualenv lol

--- Python/Scripts/test.bat
+++
@@ -1,5 +0,0 @@
-C:\Users\Jasper-school\AppData\Local\Programs\Python\Python37\python.exe challenge.py -c 5 -t 20
-echo 'all noeqs completed'
-C:\Users\Jasper-school\AppData\Local\Programs\Python\Python37\python.exe challenge.py -c 10 -t 9
-echo 'all nongu's complete'
-C:\Users\Jasper-school\AppData\Local\Programs\Python\Python37\python.exe guffin.py
