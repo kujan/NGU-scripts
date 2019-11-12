2175a07c1e41bd5489c950a0033a6dad46aa3597
Fredrik Kujansuu <fredrik.k91@gmail.com>
2019-11-12 17:21:18 +0100

    some clarifications

--- scripts/glop.py
+++ scripts/glop.py
@@ -1,15 +1,15 @@
-"""Challenge start script."""
+"""Glop farming script."""
+import datetime
 import time
-
+from typing import NamedTuple
 # Helper classes
 from classes.features import Adventure
 from classes.helper   import Helper
 from classes.inputs import Inputs
-import coordinates as coords
 from classes.navigation import Navigation
 from classes.window import Window
+import coordinates as coords
 import usersettings as userset
-from typing import NamedTuple
 
 
 class Reagent(NamedTuple):
@@ -19,11 +19,10 @@ class Reagent(NamedTuple):
     page: int
 
 class Glop:
-    
     inv_pages_unlocked = 0
     reagents = {}
     target = 10
-
+    start = 0
     GLOP_ZONE_MAP = {"ice_cream.png": 8,
                      "steak.png": 16,
                      "surstromming.png": 21,
@@ -32,7 +31,9 @@ class Glop:
                      }
 
     @staticmethod
-    def init(target):
+    def init(target: int) -> None:
+        """Counts available inventory pages."""
+        Glop.start = time.time()
         Glop.target = target
         Navigation.menu("inventory")
         for btn in coords.INVENTORY_PAGE:
@@ -42,7 +43,8 @@ class Glop:
         Glop.update_inventory()
 
     @staticmethod
-    def update_inventory():
+    def update_inventory() -> None:
+        """Scan all inventory pages for glop related items."""
         Navigation.menu("inventory")
         for item in coords.GLOP_FILENAMES: Glop.reagents[item] = []
 
@@ -64,7 +66,7 @@ class Glop:
             print(f"{item}: {len(Glop.reagents[item])}")
 
     @staticmethod
-    def loop():
+    def loop() -> None:
         while len(Glop.reagents["glop.png"]) < Glop.target:
             Navigation.menu("inventory")
             # Find the glop reagent we have the fewest of
@@ -76,11 +78,13 @@ class Glop:
             print(f"converted {len(Glop.reagents[target])} glops")
             Adventure.snipe(Glop.GLOP_ZONE_MAP[target], 2)
             Glop.update_inventory()
+        elapsed = round(time.time() - Glop.start)
+        print(f"Finished collecting {Glop.target} glops in {datetime.timedelta(seconds=elapsed)}")
 
 if __name__ == "__main__":
     Helper.init(True)
     Helper.requirements()
-    print("How many glops do you wish to farm?")
+    print("How many glops do you wish to farm? The script will farm UP TO this amount:")
     target = int(input())
     Glop.init(target)
     Glop.loop()
